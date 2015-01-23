0b67698761e99eaa4a00f5ed16d943ceea96bc07
Ali Smith <alipsmith@me.com>
2015-01-23 13:14:57 -0800

    cleaned up, lost semi-colons in else stmnts

--- app.js
+++ app.js
@@ -1,8 +1,5 @@
 // To complete this project, you’ll need to use loops, conditionals and 
 // the module operator. 
-// you just need to inject the each fizz/buzz/fizzbuzz/number 
-// into an <li> and append them to the <ul> using a $ selector
-
 
 var list = 
 document.getElementById("list"); //gets ul ("list") from DOM
@@ -13,18 +10,18 @@ for (var i = 1; i < 101; i++) { // for loop counts to 100
 	
 	if ((i % 3 == 0) && (i % 5 == 0)) {
 		$('#list').append('<li><p>fizzbuzz</p></li>'); //condition: if both i/3 = 0 and i/5 = 0, return 'fizzbuzz'
-	};
+	}
 	
 	else if (i % 3 == 0) {
 		$('#list').append('<li><p>fizz</p></li>'); // condition: if i/3 = 0, then return 'fizz' 
-	};
+	}
 
 	else if (i % 5 == 0) {
 		$('#list').append('<li><p>buzz</p></li>');//condition: if i/5 = 0, then return 'buzz' 
-	};
+	}
 
 	else {
-		$('#list').append('<li>i</li>');//if none of the conditions are met, return the integer
+		$('#list').append('<li>' +i + '</li>');//if none of the conditions are met, return the integer
 		};
 
 };//end for function
