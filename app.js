10eafdd92072859e59518d1a7108f6e61225a138
Ali Smith <alipsmith@me.com>
2015-01-23 12:42:39 -0800

    added quotes

--- app.js
+++ app.js
@@ -4,7 +4,6 @@
 // into an <li> and append them to the <ul> using a $ selector
 
 
-
 var list = 
 document.getElementById("list"); //gets ul ("list") from DOM
 
@@ -13,19 +12,19 @@ var i;// the number that constitutes the <li>
 for (var i = 1; i < 101; i++) { // for loop counts to 100
 	
 	if ((i % 3 == 0) && (i % 5 == 0)) {
-		$(#list).append(<li><p>fizzbuzz</p></li>); //condition: if both i/3 = 0 and i/5 = 0, return 'fizzbuzz'
+		$(#list).append('<li><p>fizzbuzz</p></li>'); //condition: if both i/3 = 0 and i/5 = 0, return 'fizzbuzz'
 	};
 	
 	else if (i % 3 == 0) {
-		$(#list).append(<li><p>fizz</p></li>); // condition: if i/3 = 0, then return 'fizz' 
+		$(#list).append('<li><p>fizz</p></li>''); // condition: if i/3 = 0, then return 'fizz' 
 	};
 
 	else if (i % 5 == 0) {
-		$(#list).append(<li><p>buzz</p></li>);//condition: if i/5 = 0, then return 'buzz' 
+		$(#list).append('<li><p>buzz</p></li>'');//condition: if i/5 = 0, then return 'buzz' 
 	};
 
 	else {
-		$(#list).append(<li>i</li>);//if none of the conditions are met, return the integer
+		$(#list).append('<li>i</li>');//if none of the conditions are met, return the integer
 		};
 
 };//end for function
